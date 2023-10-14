3a9ee153b0330b13bb493f22cbe1daf326a04e1a
Natia Khizanishvili <nati.khizanishvili@gmail.com>
2023-10-15 01:55:23 +0400

    update assignment 10

--- TBC Academy/TBC Academy/Assignment 10.playground/Contents.swift
+++ TBC Academy/TBC Academy/Assignment 10.playground/Contents.swift
@@ -1,61 +1,63 @@
-//-ControlCenter-ი.
-//With properties: isLockedDown: Bool და securityCode: String, რომელშიც იქნება რაღაც პაროლი შენახული.
-//Method lockdown, რომელიც მიიღებს პაროლს, ჩვენ დავადარებთ ამ პაროლს securityCode-ს და თუ დაემთხვა გავაკეთებთ lockdown-ს.
-//Method-ი რომელიც დაგვიბეჭდავს ინფორმაციას lockdown-ის ქვეშ ხომ არაა ჩვენი ControlCenter-ი.
-//
+// 1-ControlCenter-ი.
+// With properties: isLockedDown: Bool და securityCode: String, რომელშიც იქნება რაღაც პაროლი შენახული.
 
-class ControlCenter {
-    var isLockedDown: Bool
+// Method lockdown, რომელიც მიიღებს პაროლს, ჩვენ დავადარებთ ამ პაროლს securityCode-ს და თუ დაემთხვა გავაკეთებთ lockdown-ს.
+
+// Method-ი რომელიც დაგვიბეჭდავს ინფორმაციას lockdown-ის ქვეშ ხომ არაა ჩვენი ControlCenter-ი.
+
+class ControlCenter: StationModule {
+    var isLockedDown = false
     let securityCode: String
     
-    init(isLockedDown: Bool, securityCode: String) {
-        self.isLockedDown = isLockedDown
+    init(securityCode: String) {
         self.securityCode = securityCode
+        
+        super.init(moduleName: "Control Center")
     }
     
-    func lockdown(password: String) {
-        if securityCode == password {
+    func lockdown(securityCode: String) {
+        if securityCode == self.securityCode {
             isLockedDown = true
         }
     }
     
-    func printIsLockdown() {
-        if isLockedDown  {
-            print("არის lockdown")
-        } else {
-            print("არ არის lockdown")
+    func printIsLockDown() {
+        if isLockedDown {
+            print("aris lockdowns") }
+        else {
+            print("ar aris lockdown") }
         }
     }
-}
 
-//2-ResearchLab-ი.
-//With properties: String Array - ნიმუშების შესანახად.
-//Method რომელიც მოიპოვებს(დაამატებს) ნიმუშებს ჩვენს Array-ში.
 
-class ResearchLab {
-    var array: [String]
+// 2-ResearchLab-ი.
+// With properties: String Array - ნიმუშების შესანახად.
+// Method რომელიც მოიპოვებს(დაამატებს) ნიმუშებს ჩვენს Array-ში.
+
+class ResearchLab: StationModule {
+    var arrey: [String] = []
     
-    init(array: [String]) {
-        self.array = array
-
+    init() {
+        super.init(moduleName: "Research Lab")
     }
-     
+    
     func add(sample: String) {
-        array.append(sample)
+        arrey.append(sample)
     }
 }
 
 
-//3-LifeSupportSystem-ა.
-//With properties: oxygenLevel: Int, რომელიც გააკონტროლებს ჟანგბადის დონეს.
-//Method რომელიც გვეტყვის ჟანგბადის სტატუსზე.
+// 3-LifeSupportSystem-ა.
+// With properties: oxygenLevel: Int, რომელიც გააკონტროლებს ჟანგბადის დონეს.
+// Method რომელიც გვეტყვის ჟანგბადის სტატუსზე.
 
-
-class LifeSupportSystem {
+class LifeSupportSystem: StationModule {
     var oxygenLevel: Int
     
     init(oxygenLevel: Int) {
         self.oxygenLevel = oxygenLevel
+       
+        super.init(moduleName: "Life Support System")
     }
     
     func getOxygenLevel() -> Int {
@@ -63,9 +65,11 @@ class LifeSupportSystem {
     }
 }
 
-//-StationModule-ი
-//With properties: moduleName: String და drone: Drone? (optional).
-//Method რომელიც დრონს მისცემს თასქს.
+
+// 4-StationModule-ი
+// With properties: moduleName: String და drone: Drone? (optional).
+// Method რომელიც დრონს მისცემს თასქს.
+
 
 class StationModule {
     var moduleName: String
@@ -76,20 +80,20 @@ class StationModule {
         self.drone = drone
     }
     
-    func giveTaskToDrone(_ task: String) {
+    func giveTaskToDrone(task: String) {
         drone?.task = task
     }
 }
+    
+// 5-ჩვენი ControlCenter-ი ResearchLab-ი და LifeSupportSystem-ა გავხადოთ StationModule-ის subClass.
+// +
 
-
-//5-ჩვენი ControlCenter-ი ResearchLab-ი და LifeSupportSystem-ა გავხადოთ StationModule-ის subClass.
-//
-
-
-//6-Drone.
-//With properties: task: String? (optional), unowned var assignedModule: StationModule, weak var missionControlLink: MissionControl? (optional).
-//Method რომელიც შეამოწმებს აქვს თუ არა დრონს თასქი და თუ აქვს დაგვიბჭდავს რა სამუშაოს ასრულებს ის.
-
+// 6-Drone.
+// With properties: 
+// task: String? (optional),
+// unowned var assignedModule: StationModule,
+// weak var missionControlLink: MissionControl? (optional).
+// Method რომელიც შეამოწმებს აქვს თუ არა დრონს თასქი და თუ აქვს დაგვიბჭდავს რა სამუშაოს ასრულებს ის.
 
 class Drone {
     var task: String?
@@ -102,18 +106,52 @@ class Drone {
         self.missionControlLink = missionControlLink
     }
     
+    func checkWork() {
+        if let task = task { // if let task
+            print("I am working on \(task)")
+        } else {
+            print("I do not have active task")
+        }
+    }
 }
 
-//7-OrbitronSpaceStation-ი შევქმნათ, შიგნით ავაწყოთ ჩვენი მოდულები ControlCenter-ი, ResearchLab-ი და LifeSupportSystem-ა. ასევე ამ მოდულებისთვის გავაკეთოთ თითო დრონი და მივაწოდოთ ამ მოდულებს რათა მათ გამოყენება შეძლონ.
-//ასევე ჩვენს OrbitronSpaceStation-ს შევუქმნათ ფუნქციონალი lockdown-ის რომელიც საჭიროების შემთხვევაში controlCenter-ს დალოქავს.
-//
-//8-MissionControl.
-//With properties: spaceStation: OrbitronSpaceStation? (optional).
-//Method რომ დაუკავშირდეს OrbitronSpaceStation-ს და დაამყაროს მასთან კავშირი.
-//Method requestControlCenterStatus-ი.
-//Method requestOxygenStatus-ი.
-//Method requestDroneStatus რომელიც გაარკვევს რას აკეთებს კონკრეტული მოდულის დრონი.
-//
+// 7-OrbitronSpaceStation-ი შევქმნათ, შიგნით ავაწყოთ ჩვენი მოდულები ControlCenter-ი, ResearchLab-ი და LifeSupportSystem-ა. 
+// ასევე ამ მოდულებისთვის გავაკეთოთ თითო დრონი და მივაწოდოთ ამ მოდულებს რათა მათ გამოყენება შეძლონ.
+// ასევე ჩვენს OrbitronSpaceStation-ს შევუქმნათ ფუნქციონალი lockdown-ის რომელიც საჭიროების შემთხვევაში controlCenter-ს დალოქავს.
+
+class OrbitronSpaceStation {
+    let controlCenter: ControlCenter
+    let researchLab: ResearchLab
+    let lifeSupportSystem: LifeSupportSystem
+    
+    let securityCode = "123456789"
+    
+    init() {
+        self.controlCenter = ControlCenter(securityCode: securityCode)
+        self.researchLab = ResearchLab()
+        self.lifeSupportSystem = LifeSupportSystem(oxygenLevel: 23)
+    }
+    
+    func lockdown() {
+        controlCenter.lockdown(securityCode: securityCode)
+    }
+}
+
+
+// 8-MissionControl.
+// With properties: spaceStation: OrbitronSpaceStation? (optional).
+// Method რომ დაუკავშირდეს OrbitronSpaceStation-ს და დაამყაროს მასთან კავშირი.
+// Method requestControlCenterStatus-ი.
+// Method requestOxygenStatus-ი.
+// Method requestDroneStatus რომელიც გაარკვევს რას აკეთებს კონკრეტული მოდულის დრონი.
+
+class MissionControl {
+    var spaceStation: OrbitronSpaceStation?
+    
+    
+        
+        
+
 //9-და ბოლოს
 //შევქმნათ OrbitronSpaceStation,
 //შევქმნათ MissionControl-ი,

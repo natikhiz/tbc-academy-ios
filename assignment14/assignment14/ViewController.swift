d79a4204eefea43e17f5c5703507b4744b916b0d
Natia Khizanishvili <nati.khizanishvili@gmail.com>
2023-10-26 01:01:08 +0400

    Start assignment 14

---
+++ assignment14/assignment14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment14
+//
+//  Created by Natia Khizanishvili on 25.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
